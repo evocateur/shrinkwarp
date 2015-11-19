948ff4cffc9c5e61078f26ad03241dcfc4bebdcc
Daniel Stockman <daniels@zillow.com>
2015-11-19 14:21:20 -0800

    more elaborate test machinations

--- fixtures/git/test.sh
+++ fixtures/git/test.sh
@@ -2,7 +2,20 @@
 
 # git checkout npm-shrinkwrap.json && shonkwrap && git diff npm-shrinkwrap.json
 
-git checkout . && \
+# clean install
+# re-rerun, no changes
+# then install new git dep
+# then upgrade new git dep
+
+rm -rf node_modules && \
+    git checkout package.json npm-shrinkwrap.json && \
+    npm i . && \
+    shonkwrap && \
+    git diff --no-ext-diff --quiet npm-shrinkwrap.json && \
     npm i -S substack/js-traverse#0.6.5 && \
     shonkwrap && \
-    git diff npm-shrinkwrap.json
+    git diff --no-ext-diff npm-shrinkwrap.json && \
+    npm i -S substack/js-traverse#0.6.6 && \
+    shonkwrap && \
+    git diff --no-ext-diff npm-shrinkwrap.json
+    # git diff --no-ext-diff --exit-code npm-shrinkwrap.json
