1b44781a80fd488d8d72a6de9d97a2053215dd9d
Daniel Stockman <daniels@zillow.com>
2015-11-19 18:22:38 -0800

    use npm version-specific directory for tests that are sensitive to shrinkwrap layout

--- test.sh
+++ test.sh
@@ -4,6 +4,14 @@ CDPATH="" # nuked to avoid wonkiness
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 EXIT_STATUS="0"
 
+npm --version
+
+NPM_VERSION=$(npm --version)
+NPM_VERSION="${NPM_VERSION%%.*}"
+
+NPM_FIXTURE="${ROOTDIR}/fixtures/${NPM_VERSION}.x"
+echo "using npm version-specific fixtures dir: ${NPM_FIXTURE}"
+
 # git checkout npm-shrinkwrap.json && shonkwrap && git diff npm-shrinkwrap.json
 
 # clean install
@@ -25,7 +33,7 @@ clean_run() {
 }
 
 manual_diffs() {
-    cd "fixtures/git"
+    cd "${NPM_FIXTURE}/git"
     rm -rf node_modules && \
         git checkout package.json npm-shrinkwrap.json && \
         npm i . && \
@@ -45,7 +53,7 @@ manual_diffs() {
 if [ "$1" = "manual" ]; then
     manual_diffs
 else
-    pushd "${ROOTDIR}/fixtures/git" && clean_run && \
+    pushd "${NPM_FIXTURE}/git" && clean_run && \
     pushd "${ROOTDIR}/fixtures/simple" && clean_run && \
     pushd "${ROOTDIR}/fixtures/tarball" && clean_run
 fi
